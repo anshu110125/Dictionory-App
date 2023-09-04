a803d3bfe35d4e239ecd39df4ddbd5de26bcf4f7
Anshu Kumar <117300557+anshu110125@users.noreply.github.com>
2023-09-04 10:41:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MainPage from './Parent/MainPage';
+import NavBar from './Parent/NavBar';
+
+function App() {
+  return (
+    <>
+    <NavBar />
+     <MainPage />
+     </>
+  );
+}
+
+export default App;
